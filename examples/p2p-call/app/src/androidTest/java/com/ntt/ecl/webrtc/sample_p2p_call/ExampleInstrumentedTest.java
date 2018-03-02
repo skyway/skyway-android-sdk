915e8c185e359caa802c5cc8cfff613aabf73af6
Tsuyoshi TAKAHASHI <tsu.takahashi@SR5-takahashi-mbp.local>
2018-03-02 18:07:40 +0900

    fix .idea files and app-name in a test code

--- examples/p2p-call/app/src/androidTest/java/com/ntt/ecl/webrtc/sample_p2p_call/ExampleInstrumentedTest.java
+++ examples/p2p-call/app/src/androidTest/java/com/ntt/ecl/webrtc/sample_p2p_call/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ntt.ecl.webrtc.sample_p2p_videochat", appContext.getPackageName());
+        assertEquals("com.ntt.ecl.webrtc.sample_p2p_call", appContext.getPackageName());
     }
 }
